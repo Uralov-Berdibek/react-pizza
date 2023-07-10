279ea6bf0c082e044c670db5f04274c14e6c037a
BerdibekUalov <uralov0212@gmail.com>
2023-07-10 16:17:21 +0500

    version 1.0.0

---
+++ src/pages/FullPizza.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useParams, useNavigate } from 'react-router-dom';
+
+const FullPizza: React.FC = () => {
+  const [pizza, setPizza] = React.useState<{
+    imageUrl: string;
+    title: string;
+    price: number;
+  }>();
+
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    async function fetchPizza() {
+      try {
+        const { data } = await axios.get('https://63cf7a0b1098240437806a35.mockapi.io/items' + id);
+        setPizza(data);
+      } catch (error) {
+        alert('Ошибка при получении пиццы!');
+        navigate('/');
+      }
+    }
+
+    fetchPizza();
+  }, [id, navigate]);
+
+  if (!pizza) {
+    return <>Загрузка...</>;
+  }
+
+  return (
+    <div className="container">
+      <img src={pizza.imageUrl} alt="pizza img" />
+      <h2>{pizza.title}</h2>
+      <h4>{pizza.price} ₽</h4>
+      <Link to="/">
+        <button className="button button--outline button--add">
+          <span>Назад</span>
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default FullPizza;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { Categories, Sort, PizzaBlock, Skeleton, Pagination } from '../components';
+
+import { useAppDispatch } from '../redux/store';
+import { selectFilter } from '../redux/filter/selectors';
+import { selectPizzaData } from '../redux/pizza/selectors';
+import { setCategoryId, setCurrentPage } from '../redux/filter/slice';
+import { fetchPizzas } from '../redux/pizza/asyncActions';
+
+const Home: React.FC = () => {
+  const dispatch = useAppDispatch();
+
+  const { items, status } = useSelector(selectPizzaData);
+  const { categoryId, sort, currentPage, searchValue } = useSelector(selectFilter);
+
+  const onChangeCategory = React.useCallback((idx: number) => {
+    dispatch(setCategoryId(idx));
+  }, [dispatch]);
+
+  const onChangePage = (page: number) => {
+    dispatch(setCurrentPage(page));
+  };
+
+  const getPizzas = async () => {
+    const sortBy = sort.sortProperty.replace('-', '');
+    const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
+    const category = categoryId > 0 ? String(categoryId) : '';
+    const search = searchValue;
+
+    dispatch(
+      fetchPizzas({
+        sortBy,
+        order,
+        category,
+        search,
+        currentPage: String(currentPage),
+      }),
+    );
+
+    window.scrollTo(0, 0);
+  };
+
+  React.useEffect(() => {
+    getPizzas();
+  }, [categoryId, sort.sortProperty, searchValue, currentPage]);
+
+  const pizzas = items.map((obj: any) => <PizzaBlock key={obj.id} {...obj} />);
+  const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
+
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Categories value={categoryId} onChangeCategory={onChangeCategory} />
+        <Sort value={sort} />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      {status === 'error' ? (
+        <div className="content__error-info">
+          <h2>Произошла ошибка 😕</h2>
+          <p>К сожалению, не удалось получить питсы. Попробуйте повторить попытку позже.</p>
+        </div>
+      ) : (
+        <div className="content__items">{status === 'loading' ? skeletons : pizzas}</div>
+      )}
+
+      <Pagination currentPage={currentPage} onChangePage={onChangePage} />
+    </div>
+  );
+};
+
+export default Home;
